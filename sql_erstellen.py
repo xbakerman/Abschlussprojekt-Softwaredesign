6d48b6da73ffdeb90d6ea2ad7e50b93b1fedbd0f
xbakerman <nm2689@mci4me.at>
2024-02-28 19:32:57 +0100

    .

--- sql_erstellen.py
+++ sql_erstellen.py
@@ -1,17 +1,8 @@
 import sqlite3
 from database_start import DatabaseConnector
 
-# Verbindung zur SQLite-Datenbank erstellen (oder Datenbank erstellen, wenn sie nicht existiert)
+
+
 conn = sqlite3.connect('my_database.db')
-
-# Cursor-Objekt erstellen
-c = conn.cursor()
-
-
-# Erstelle einen Index auf der 'hash' Spalte in der 'hashes' Tabelle
-c.execute("CREATE INDEX hash_index ON hashes (hash)")
-
-
-    # Änderungen speichern und Verbindung schließen
-conn.commit()
+conn.execute('VACUUM')
 conn.close()
