d6936adbed6e7ee6878817b77bbad3363107248c
Markusbra <@mcime.atbm>
2024-03-01 16:19:58 +0100

    Erweiterung youtube

--- youtube_integration.py
+++ youtube_integration.py
@@ -1,16 +1,15 @@
 import requests 
+import re
 from urllib.parse import quote_plus
 
 def link_youtube(title, artist):
     query = f"{title} {artist} official music video"
     url = f"https://www.youtube.com/results?search_query={quote_plus(query)}"
     response = requests.get(url)
+
     if response.status_code == 200:
-        video = None
-        start_index = response.text.find('{"videoRenderer":{"videoId":"')
-        if start_index != -1:
-            end_index = response.text.find('"', start_index + 30)
-            video = response.text[start_index + 29:end_index]
-        if video:
-            return f"https://www.youtube.com/watch?v={video}"
+        match = re.search(r'{"videoRenderer":{"videoId":"([^"]+)"', response.text)
+        if match:
+            video_id = match.group(1)
+            return f"https://www.youtube.com/watch?v={video_id}"
     return None
